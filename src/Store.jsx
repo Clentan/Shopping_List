c9be7354b6764a18cd7bb1a2d78d6c921ad70ff6
Clentan <clentanchauke9089@gmail.com>
2024-11-21 15:46:02 +0200

    Final

--- src/Store.jsx
+++ src/Store.jsx
@@ -163,7 +163,7 @@ export default function Store() {
     <div>
       <section className="bg-white">
         <div className="grid grid-cols-1 lg:grid-cols-2">
-          <div className="relative flex items-end px-4 pb-10 pt-60 sm:pb-16 md:justify-center lg:pb-24 bg-gray-50 sm:px-6 lg:px-8">
+          <div className="relative flex items-end px-4  pb-10 pt-60 sm:pb-16 md:justify-center lg:pb-24 bg-gray-50 sm:px-6 lg:px-8">
             <div className="absolute inset-0">
               <img
                 className="object-cover w-full h-full"
